8682402828769de7796fb2103af3c0bb61e57bd5
Philippe O. Wagner <wagner@arteria.ch>
2015-07-20 00:03:13 +0200

    added requirements

--- tests/runtests.py
+++ tests/runtests.py
@@ -5,58 +5,32 @@ app will be developed and tested outside any specifiv Django project.
 Via ``settings.configure`` you will be able to set all necessary settings
 for your app and run the tests as if you were calling ``./manage.py test``.
 """
-
 import sys
-import django
+
 from django.conf import settings
 
+from smart_load_tag.tests import test_settings
 
-def setup():
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:'
-        }
-    }
+if not settings.configured:
+    settings.configure(**test_settings.__dict__)
 
-    INSTALLED_APPS = [
-        'smart_load_tag',
-    ]
-    if django.VERSION >= (1,6):
-        TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-    else:
-        TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
-
-    from django.conf import settings
-
-    if not settings.configured:
-        settings.configure(
-            INSTALLED_APPS=INSTALLED_APPS,
-            DATABASES=DATABASES,
-            TEST_RUNNER=TEST_RUNNER,
-            ROOT_URLCONF='smart_load_tag.testproject.urls'
-        )
+from django_nose import NoseTestSuiteRunner
 
 
+class NoseCoverageTestRunner(NoseTestSuiteRunner):
+    """Custom test runner that uses nose and coverage"""
+    pass
 
 
 def runtests(*test_args):
-
-    if django.VERSION >= (1,7):
+    import django
+    if django.VERSION >= (1, 7):
         django.setup()
-
-
-    from django.test.utils import get_runner
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=1, interactive=False, failfast=False)
-
-    failures = test_runner.run_tests(test_args)
-
+    failures = NoseCoverageTestRunner(verbosity=2, interactive=True).run_tests(test_args)
     sys.exit(failures)
 
 
 if __name__ == '__main__':
-    setup()
     runtests(*sys.argv[1:])
     
     
